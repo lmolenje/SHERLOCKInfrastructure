c959fae4ee3942f2bd1f9d7ba5ea2fec52f0a2dc
2020TechScenario <65263074+2020TechScenario@users.noreply.github.com>
2020-05-12 22:04:34 -0400

    Update and rename InitialInstall.sh to ControlNodeInitialInstall.sh

---
+++ Ansible/ControlNodeInitialInstall.sh
@@ -0,0 +1,4 @@
+echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' >> /etc/apt/sources.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+sudo apt install -y ansible
+
